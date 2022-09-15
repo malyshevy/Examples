b2657c6510e0a8a62abea36ca8cc0f5242e2dbd8
Yuri Malyshev <team21_07@list.ru>
2022-09-15 16:03:25 +0300

    string 1-4

--- Example002_HelloUser/Program.cs
+++ Example002_HelloUser/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите имя пользователя");
+string userName = Console.ReadLine();
+Console.WriteLine("Hello");
+Console.WriteLine(userName);

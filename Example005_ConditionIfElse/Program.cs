f1d138ddcab02f7e95f98d091fddbf004f8709cf
Yuri Malyshev <team21_07@list.ru>
2022-09-15 17:39:07 +0300

    add string 1-12

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите имя пользователя");
+string nameA;
+nameA = Console.ReadLine();
+if (nameA.ToLower() =="маша")
+    {
+        Console.WriteLine("URA!!! This MASHA!!!");
+    }
+else
+    {
+        Console.WriteLine("Hi");
+        Console.WriteLine(nameA);
+    }
